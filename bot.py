e434c2c96a763a16adf8f158bdc7d89a3d93495b
Divyansh Singh <divyanshanandsingh@gmail.com>
2021-07-19 13:42:11 +0530

    main push

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 # pyright: reportMissingImports=false, reportUnusedVariable=warning, reportUntypedBaseClass=error
 import discord;
 import os
-
-from discord import activity;
+import requests;
 from dotenv import load_dotenv
-#for the environment variables, in this case, the key
+
+#for the environment variables, in this case, the key   
 load_dotenv();
 
 bot = discord.Client();
 
 key = os.environ['API_KEY']
+meme_api = 'https://meme-api.herokuapp.com/gimme/'
+
 
 @bot.event
 async def on_ready():
@@ -24,7 +26,12 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('hello');
         print('Said Hello in', message.channel,' of ', message.guild);
-    if message.content.startswith('$meme'): 
-        await message.channel.send('I\'m still working on this');
-
+        
+    if message.content.startswith('$reddit'): 
+        subreddit = message.content[7:].strip();
+        res = requests.get(meme_api+subreddit);
+        await message.channel.send(res.json()['preview'][len(res.json()['preview'])-1]);
+        await message.channel.send(res.json()['title']);
+        print('picture from',res.json()['subreddit'], 'sent in ', message.guild);
+        
 bot.run(key)
