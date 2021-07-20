8f63ba9486f24a7e74a921337c091f6205e3e199
Rehaan Sahay <34886298+sayheyreh@users.noreply.github.com>
2021-07-21 01:11:51 +0530

    added , to give bored users activites to do

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # pyright: reportMissingImports=false, reportUnusedVariable=warning, reportUntypedBaseClass=error
 import discord;
 import os
+from discord import channel
+from discord import activity
 import requests;
 import random;
 from dotenv import load_dotenv
@@ -13,6 +15,7 @@ bot = discord.Client();
 key = os.environ['API_KEY']
 meme_api = 'https://meme-api.herokuapp.com/gimme/'
 joke_api =  'https://official-joke-api.appspot.com/jokes/random'
+bored_api = 'http://www.boredapi.com/api/'
 
 def randColour():
     return discord.Colour.from_rgb(random.randint(128, 255),random.randint(128, 255),random.randint(128, 255));
@@ -26,6 +29,14 @@ def getInfo(u):
             e.add_field(name='created_on',value=u.created_at,inline=False);
             e.add_field(name='avatar',value=u.avatar_url,inline=False);
             return e;
+activityTypes = ['education','recreational','social','diy','charity','cooking','relaxation','music','busywork']
+
+def boredEmbed(user,res):
+    e=discord.Embed(title=res['activity'],color=randColour())
+    e.set_author(name=(user.display_name+'#'+user.discriminator),icon_url=user.avatar_url)
+    e.set_footer(text=res['type'])
+    return e;
+
 @bot.event
 async def on_ready():
     print('logged in as {0.user}'.format(bot));
@@ -66,5 +77,20 @@ async def on_message(message):
         user=message.author;
         e.set_author(name=(user.display_name+'#'+user.discriminator),icon_url=user.avatar_url);
         await message.channel.send(embed=e);
-        
+    if message.content.startswith('$bored'):
+        def check(m):
+            return m.content!=None and m.channel==message.channel and m.author==message.author;
+        await message.channel.send('Any type you prefer?\n`education`,`recreational`,`social`,`diy`,`charity`,`cooking`,`relaxation`,`music`,`busywork`\nif you do not have a preference, type `no`');
+        msg = await bot.wait_for('message',check=check)
+        user=message.author;
+        if msg.content.lower() in activityTypes:
+            res=requests.get(bored_api+'activity?type='+msg.content).json();
+            e=boredEmbed(user,res)
+            await message.channel.send(embed=e);
+        elif msg.content.lower()=='no':
+            res=requests.get(bored_api+'activity').json();
+            e=boredEmbed(user,res)
+            await message.channel.send(embed=e)
+        else:
+            await message.channel.send('Sorry Invalid Input')
 bot.run(key)
